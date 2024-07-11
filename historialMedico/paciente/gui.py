b63a30b95d027ba0d9d7d28be060b0252e15d1cd
KruseCyrux <cracbv@gmail.com>
2024-07-10 22:08:41 -0600

    commit 1

---
+++ historialMedico/paciente/gui.py
@@ -0,0 +1,9 @@
+import tkinter as tk 
+
+class Frame(tk.Frame):
+    def __init__(self, root):
+
+        super().__init__(root, width=1280, height=720)
+        self.root = root
+        self.pack()
+        self.config(bg='#CDD8FF')
